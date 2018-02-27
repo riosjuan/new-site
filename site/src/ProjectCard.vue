d7ccde3d63f276b7b163edca9db151ea31d55897
Juan Rios <rios@ngti.nl>
2018-02-27 10:37:08 +0100

    Updates

---
+++ site/src/ProjectCard.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  <h1>{{ header }}</h1>
+  <hr>
+  <button @click='changeHeader'>Change header</button>
+</div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      header: 'Project Title'
+    };
+  },
+  methods: {
+    changeHeader() {
+      this.header = 'Something else';
+    }
+  }
+};
+</script>
--- site/src/main.js
+++ site/src/main.js
@@ -1,7 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import Home from './Home.vue';
+
+Vue.component('site-header', Home);
 
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
