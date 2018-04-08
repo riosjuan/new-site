077083da44a881e4fed1fc24ce75fead4a22696b
Juan Rios <rios@ngti.nl>
2018-04-08 23:06:29 +0200

    Added Vue to project

---
+++ src/components/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <div>
+      <h1>Juan Rios</h1>
+      <h4>Product designer</h4>
+      <p>
+        I'm passionate about visual design, meaningful interactions and engaging user experiences.
+      </p>
+    </div>
+    <div class="project-cards">
+      <app-project-card v-for="ProjectCard in ProjectCards" :project-card="ProjectCard"
+                        :key="ProjectCard.name"></app-project-card>
+    </div>
+  </div>
+</template>
+
+<script>
+import ProjectCard from './ProjectCard';
+
+export default {
+  components: {
+    'app-project-card': ProjectCard,
+  },
+  data() {
+    return {
+      ProjectCards: [
+        { name: 'Llorona Records', description: 'Responsive web design and front-end development' },
+        { name: 'Swisscom iO', description: 'Visual and user experience design for Android' },
+        { name: 'Simple Rain', description: 'Visual and user experience design for iOS (iPhone)' },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+  .container {
+    padding: 4rem 2rem;
+  }
+  .project-cards {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-column-gap: 2rem;
+  }
+</style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card">
+    <h2>{{ ProjectCard.name }}</h2>
+    <p>{{ ProjectCard.description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    ProjectCard: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.card {
+  padding: 1rem;
+  box-shadow: 0 4px 8px 0 rgba(23,26,33,0.1);
+}
+</style>
