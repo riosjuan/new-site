bfa8b6fc5e618c6591b394a702b4fe3ac4e7866e
Juan Rios <rios@ngti.nl>
2018-04-13 22:21:30 +0200

    Removed vux and store

--- src/router/index.js
+++ src/router/index.js
@@ -15,7 +15,7 @@ export default new Router({
     },
     {
       path: ':ProjectName',
-      name: 'ProjectName',
+      name: 'project-name',
       component: Projects,
       props: true,
     },
